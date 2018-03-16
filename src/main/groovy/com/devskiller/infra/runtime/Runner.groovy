e72a7c1a792892bb37dc39486f56c1d141687cbf
jkubrynski <jkubrynski@gmail.com>
2018-03-17 00:36:47 +0100

    Add automatic imports for all classes under main package

--- src/main/groovy/com/devskiller/infra/runtime/Runner.groovy
+++ src/main/groovy/com/devskiller/infra/runtime/Runner.groovy
@@ -1,9 +1,16 @@
 package com.devskiller.infra.runtime
 
+import java.nio.file.FileSystems
+import java.nio.file.FileVisitResult
 import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.PathMatcher
 import java.nio.file.Paths
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
 
 import org.codehaus.groovy.control.CompilerConfiguration
+import org.codehaus.groovy.control.customizers.ImportCustomizer
 
 import com.devskiller.infra.azure.Infrastructure
 
@@ -14,7 +21,45 @@ class Runner {
 	}
 
 	private static GroovyShell groovyShell() {
-		return new GroovyShell(Runner.classLoader, new CompilerConfiguration(sourceEncoding: 'UTF-8'))
+		CompilerConfiguration compilerConfiguration = new CompilerConfiguration(sourceEncoding: 'UTF-8')
+		ImportCustomizer importCustomizer = new ImportCustomizer()
+
+		listClassNamesInPackage('com.devskiller.infra.azure').each {
+			clazz -> importCustomizer.addImports(clazz)
+		}
+
+		compilerConfiguration.addCompilationCustomizers(importCustomizer)
+		return new GroovyShell(Runner.classLoader, compilerConfiguration)
+	}
+
+	static List<String> listClassNamesInPackage(String packageName) throws Exception {
+		List<String> classes = new ArrayList<>()
+		Enumeration<URL> resources = Thread.currentThread().getContextClassLoader().getResources(packageName.replace('.' as char, File.separatorChar))
+		if (!resources.hasMoreElements()) {
+			throw new IllegalStateException("No package found: " + packageName)
+		}
+		PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:*.class")
+		while (resources.hasMoreElements()) {
+			URL resource = resources.nextElement()
+			Files.walkFileTree(Paths.get(resource.toURI()), new SimpleFileVisitor<Path>() {
+				@Override
+				FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+					if (pathMatcher.matches(path.getFileName())) {
+						try {
+							String className = Paths.get(resource.toURI()).relativize(path).toString().replace(File.separatorChar, '.' as char)
+							String fullName = Class.forName(packageName + '.' + className.substring(0, className.length() - 6)).getCanonicalName()
+							if (fullName) {
+								classes << fullName
+							}
+						} catch (URISyntaxException e) {
+							throw new IllegalStateException(e)
+						}
+					}
+					return FileVisitResult.CONTINUE
+				}
+			})
+		}
+		return classes
 	}
 
 	static void main(String[] args) {
